129c9195776df80cebc3874dc2b2f98b68a9b368
Arkadiusz Kubiszewski <arek.kubiszewski@gmail.com>
2023-03-26 14:18:10 +0200

    created main.cs file

---
+++ mian.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+//The app will be a simple budget tracking solution incorporating given requirements
+//
+//Credits: Arkadiusz Kubiszewski https://github.com/Shift-Happens/
+
+namespace BudgetTracker 
+{
+    class Main 
+    { 
+        
+    }
+}
